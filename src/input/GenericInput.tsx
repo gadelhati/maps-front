859977c0b6559c1de19552c5a38247bc70b30ecb
1SG-HN Gadelha <marcelo.gadelha@mairnha.mil.br>
2023-10-04 16:23:51 -0300

    inputgroup inhanced

--- src/input/GenericInput.tsx
+++ src/input/GenericInput.tsx
@@ -15,13 +15,13 @@ export const GenericInput = (group: any) => {
             </div>
             <div className={index === 1 ? 'inputgroup' : 'hide'}>
                 <span className='prefix'><input hidden={group.hidden} type="checkbox" />1{group.prefix && group.prefix}</span>
-                <input type={group.type}></input>
+                <input type={group.type} placeholder=''></input>
                 <span className='suffix'>{group.suffix}</span>
             </div>
 
             <div className={index === 2 ? 'inputgroup' : 'hide'}>
                 <span className='prefix'><input hidden={group.hidden} type="checkbox" />2{group.prefix && group.prefix}</span>
-                <input type={group.type}></input>
+                <input type={"number"} placeholder=''></input>
                 <span className='suffix'>{group.suffix}</span>
             </div>
         </>
