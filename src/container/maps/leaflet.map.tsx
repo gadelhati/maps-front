af800d9d16ca582733676cb5b41ae4f712c6584c
GadelhaTI <gadelha.ti@gmail.com>
2024-03-30 01:27:46 -0300

    bug: set toggle items

--- src/container/maps/leaflet.map.tsx
+++ src/container/maps/leaflet.map.tsx
@@ -4,19 +4,21 @@ import 'leaflet/dist/leaflet.css';
 import { retrieve } from '../../service/service.crud';
 import { UriToScreenFormat } from '../../assets/uri.format';
 import { GaugeStation } from '../../component/gauge_station/gauge_station.interface';
+import { addFeatureGroup as addLayerGroup, addOverlay, addPolygon } from './addItem';
 
 export const LeafletMap = () => {
-    const center: L.LatLngExpression = [40.799311, -74.118464]
+    const center: L.LatLngExpression = [-23, -43]
     const [ map, setMap ] = useState<any>()
     const [ markers, setMarkers ] = useState<L.LayerGroup>()
     const [ overlay, setOverlay ] = useState<L.ImageOverlay>()
-    const [ show, setShow ] = useState<boolean>(true)
+    const [ polygon, setPolygon ] = useState<L.Polygon>()
+    const [ show, setShow ] = useState<boolean[]>([true, true, true])
     const [ tile, setTile ] = useState<boolean>(true)
 
     useEffect(()=>{
         let base = L.map('map').setView(center, 13)
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
+            maxZoom: 8,
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }).addTo(base)
         setMap(base)
@@ -24,71 +26,57 @@ export const LeafletMap = () => {
             base.remove()
         }
     }, [])
-    const toggleShow = () => {
-        setShow(!show)
+    const toggleShow = (index: number) => {
+        let value = show.slice()
+        value[index] = !show[index]
+        setShow(value)
     }
-    const toggleTile = (base: any) => {
-        setTile(!tile)
-        if (tile) {
-            return L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                maxZoom: 19,
-                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-            }).addTo(base)
-        } else {
-            return L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-                maxZoom: 19,
-                attribution: '© OpenStreetMap contributors, Tiles style by Humanitarian OpenStreetMap Team hosted by OpenStreetMap France'
-            }).addTo(base)
-        }
-    }
+    // const toggleTile = (base: any) => {
+    //     setTile(!tile)
+    //     if (tile) {
+    //         return L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    //             maxZoom: 19,
+    //             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    //         }).addTo(base)
+    //     } else {
+    //         return L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    //             maxZoom: 19,
+    //             attribution: '© OpenStreetMap contributors, Tiles style by Humanitarian OpenStreetMap Team hosted by OpenStreetMap France'
+    //         }).addTo(base)
+    //     }
+    // }
     const retrieveItem = async(url: string) => {
         let vector: GaugeStation[] = []
         await retrieve(url, 0, 1000, 'title', undefined)
             .then((data: any) => {
                 vector = data.content
             });
-        add(vector)
+        itemMarkers(vector)
     }
-    const add = (vector: GaugeStation[]) => {
-        toggleShow()
-        let layerGroup: L.LayerGroup = L.layerGroup()
-        vector.forEach((element: GaugeStation)=>{
-            layerGroup.addLayer(L.marker(element.point.coordinates.slice(0).reverse()).bindPopup(element.title))
-        })
-        if(!map.hasLayer(layerGroup)) { setMap(map.addLayer(layerGroup)) }
-        setMarkers(layerGroup)
-        map.fitBounds(layerGroup.getBounds());
+    const itemMarkers = (vector: GaugeStation[]) => {
+        toggleShow(0)
+        setMarkers(addLayerGroup(map, vector))
     }
-    const remove = () => {
-        toggleShow()
-        map.removeLayer(markers)
+    const itemOverlay = () => {
+        toggleShow(1)
+        setOverlay(addOverlay(map))
     }
-    const overlayImage = () => {
-        var polygon = L.imageOverlay('/public/chart/25110.png', L.latLngBounds([[-60.75, -53.5], [-62.08, -57.75]]), {
-            opacity: 0.6,
-            errorOverlayUrl: 'https://cdn-icons-png.flaticon.com/512/110/110686.png',
-            alt: 'Image of Newark, N.J. in 1922. Source: The University of Texas at Austin, UT Libraries Map Collection.',
-            interactive: true
-        }).addTo(map)
-        setOverlay(polygon)
-        map.fitBounds(polygon.getBounds());
+    const itemPolygon = () => {
+        toggleShow(2)
+        setPolygon(addPolygon(map))
     }
-    const removeOverlay = () => {
-        // toggleShow()
-        map.removeLayer(overlay)
-    }
-    const addPolygon = () => {
-        var polygon = L.polygon([[-28.6, -48.8166666666666667], [-31, -43], [-26, -38], [-23.0166666666666667, -42], [-28.6, -48.8166666666666667]], {color: 'red'}).addTo(map);
-        if(!map.hasLayer(polygon)) { setMap(map.addLayer(polygon)) }
-        map.fitBounds(polygon.getBounds());
+    const remove = (index: number, element: any) => {
+        toggleShow(index)
+        map.removeLayer(element)
     }
     return (
         <div className='groupbutton'>
-            <button hidden={!show} onClick={()=>retrieveItem('gauge_station')}>{UriToScreenFormat('gauge')}</button>
-            <button hidden={show} onClick={remove}>{UriToScreenFormat('remove')}</button>
-            <button onClick={overlayImage}>{UriToScreenFormat('overlay')}</button>
-            <button onClick={removeOverlay}>{UriToScreenFormat('remove overlay')}</button>
-            <button onClick={addPolygon}>{UriToScreenFormat('polygon')}</button>
+            <button hidden={!show[0]} onClick={()=>retrieveItem('gauge_station')}>{UriToScreenFormat('gauge')}{show[0]}</button>
+            <button hidden={show[0]} onClick={()=>remove(0, markers)}>{UriToScreenFormat('rm gauge')}{show[0]}</button>
+            <button hidden={!show[1]} onClick={itemOverlay}>{UriToScreenFormat('overlay')}{show[1]}</button>
+            <button hidden={show[1]} onClick={()=>remove(1, overlay)}>{UriToScreenFormat('rm overlay')}{show[1]}</button>
+            <button hidden={!show[2]} onClick={itemPolygon}>{UriToScreenFormat('polygon')}{show[2]}</button>
+            <button hidden={show[2]} onClick={()=>remove(2, polygon)}>{UriToScreenFormat('rm polygon')}{show[2]}</button>
             <div id='map' style={{ height: '100vh', width: '100vw' }}></div>
         </div>
     )
