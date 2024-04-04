899a8d1dffee4278e24cfa38645438680ea7929e
Gadelha TI <marcelo.gadelha@mairnha.mil.br>
2024-04-04 15:55:35 -0300

    bug: change state id to uuid

--- src/component/state/state.initial.tsx
+++ src/component/state/state.initial.tsx
@@ -2,6 +2,7 @@ import { State } from "./state.interface";
 
 export const initialState : State = {
     id: '',
+    code: '',
     name: '',
     country: undefined,
 }
--- src/component/state/state.interface.tsx
+++ src/component/state/state.interface.tsx
@@ -2,6 +2,7 @@ import { Country } from "../country/country.interface";
 
 export interface State {
     readonly id: string,
+    code: string,
     name: string,
     country?: Country,
 }
