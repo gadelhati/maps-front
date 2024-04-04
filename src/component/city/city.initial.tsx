09cf6d7b4be82ab22aaf66f23d8280700c43b5cc
Gadelha TI <marcelo.gadelha@mairnha.mil.br>
2024-04-04 14:43:05 -0300

    bug: change city id to uuid

--- src/component/city/city.initial.tsx
+++ src/component/city/city.initial.tsx
@@ -2,6 +2,7 @@ import { City } from "./city.interface";
 
 export const initialCity : City = {
     id: '',
+    code: '',
     name: '',
     state: undefined,
 }
--- src/component/city/city.interface.tsx
+++ src/component/city/city.interface.tsx
@@ -2,6 +2,7 @@ import { State } from "../state/state.interface";
 
 export interface City {
     readonly id: string,
+    code: string,
     name: string,
     state?: State,
 }
