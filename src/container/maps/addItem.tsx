546f4f4b7fd9c3a1522ea304c738358cb79fab81
GadelhaTI <gadelha.ti@gmail.com>
2024-04-10 02:25:04 -0300

    feature: add chart by index

--- src/container/maps/addItem.tsx
+++ src/container/maps/addItem.tsx
@@ -1,22 +1,19 @@
 import * as L from 'leaflet'
 import { GaugeStation } from '../../component/gauge_station/gauge_station.interface';
 
-export const addOverlay = (map: L.Map) => {
-    var item = L.imageOverlay('/public/chart/25110.svg', L.latLngBounds([[-60.75, -53.5], [-62.08, -57.75]]), {
+export const addOverlay = (ne: any, sw: any) => {
+    return L.imageOverlay('/public/chart/25110.svg', L.latLngBounds([ne, sw]), {
         opacity: 0.6,
         errorOverlayUrl: 'https://cdn-icons-png.flaticon.com/512/110/110686.png',
         alt: 'Image of Newark, N.J. in 1922. Source: The University of Texas at Austin, UT Libraries Map Collection.',
-        interactive: true
+        // interactive: true
     })
-    map.addLayer(item)
-    map.fitBounds(item.getBounds());
-    return item
 }
 
 export const addPolygon = (map: L.Map) => {
     var polygon = L.polygon([[-28.6, -48.8166666666666667], [-31, -43], [-26, -38], [-23.0166666666666667, -42], [-28.6, -48.8166666666666667]], {color: 'red'});
     map.addLayer(polygon)
-    map.fitBounds(polygon.getBounds());
+    map.fitBounds(polygon.getBounds())
     return polygon
 }
 
@@ -27,6 +24,6 @@ export const addFeatureGroup = (map: L.Map, vector: GaugeStation[]) => {
             featureGroup.addLayer(L.marker(element.point.coordinates.slice(0).reverse()).bindPopup(element.title))
     })
     map.addLayer(featureGroup)
-    map.fitBounds(featureGroup.getBounds());
+    map.fitBounds(featureGroup.getBounds())
     return featureGroup
 }
--- src/container/maps/leaflet.map.tsx
+++ src/container/maps/leaflet.map.tsx
@@ -6,15 +6,19 @@ import { UriToScreenFormat } from '../../assets/uri.format';
 import { GaugeStation } from '../../component/gauge_station/gauge_station.interface';
 import { addFeatureGroup , addOverlay, addPolygon } from './addItem';
 import './leaflet.css'
+import { Chart } from '../../component/chart/chart.interface';
 
 export const LeafletMap = () => {
     const center: L.LatLngExpression = [-23, -43]
     const [ map, setMap ] = useState<any>()
     const [ markers, setMarkers ] = useState<L.LayerGroup>()
     const [ overlay, setOverlay ] = useState<L.ImageOverlay>()
+    const [ overlays, setOverlays ] = useState<L.ImageOverlay[]>([])
+    const [ charts, setCharts ] = useState<Chart[]>([])
     const [ polygon, setPolygon ] = useState<L.Polygon>()
     const [ show, setShow ] = useState<boolean[]>([true, true, true])
     const [ tile, setTile ] = useState<boolean>(true)
+    let item: L.ImageOverlay
 
     useEffect(()=>{
         let base = L.map('map').setView(center, 13)
@@ -24,6 +28,7 @@ export const LeafletMap = () => {
         }).addTo(base)
         setMap(base)
         return () => {
+            retrieveCharts('chart', 'number')
             base.remove()
         }
     }, [])
@@ -54,13 +59,46 @@ export const LeafletMap = () => {
             });
         itemMarkers(vector)
     }
+    const retrieveCharts = async(url: string, sort: string) => {
+        await retrieve(url, 0, 1000, sort, undefined)
+            .then((data: any) => {
+                // console.log(data.content)
+                setCharts(data.content)
+            });
+    }
+    const retrieveChart = async(index: number) => {
+        let vector: L.ImageOverlay[] = []
+        vector[index] = addOverlay(charts[index].ne.coordinates.slice(0), charts[index].sw.coordinates.slice(0))
+        setOverlays(vector)
+        console.log(vector)
+        if(map.hasLayer(vector[index])){
+            console.log('sim', vector[index])
+            map.removeLayer(vector[index])
+        } else {
+            console.log('não', vector[index])
+            map.addLayer(vector[index])
+            map.fitBounds(vector[index].getBounds())
+        }
+    }
     const itemMarkers = (vector: GaugeStation[]) => {
         toggleShow(0)
         setMarkers(addFeatureGroup(map, vector))
     }
-    const itemOverlay = () => {
-        toggleShow(1)
-        setOverlay(addOverlay(map))
+    const itemOverlay = async(index: number) => {
+        toggleShow(1)       
+        // await retrieve('chart')
+        //     .then((data: any) => {
+                // item = addOverlay(data.content[index].ne.coordinates.slice(0), data.content[index].sw.coordinates.slice(0))
+                // setOverlay(overlays[index])
+                if(map.hasLayer(overlays[index])){
+                    console.log('sim', overlays[index])
+                    map.removeLayer(overlays[index])
+                } else {
+                    console.log('não', overlays[index])
+                    map.addLayer(overlays[index])
+                    map.fitBounds(overlays[index].getBounds())
+                }                
+            // });
     }
     const itemPolygon = () => {
         toggleShow(2)
@@ -74,15 +112,16 @@ export const LeafletMap = () => {
         <div>
             <div id='map' style={{ height: '100vh', width: '100vw' }}></div>
             <div className='menu'>
+                <button onClick={()=>retrieveChart(1)}>123</button>
                 <button hidden={!show[0]} onClick={()=>retrieveItem('gauge_station', 'title')}>{UriToScreenFormat('gauge')}{show[0]}</button>
                 <button hidden={show[0]} onClick={()=>remove(0, markers)}>{UriToScreenFormat('rm gauge')}{show[0]}</button>
-                <button hidden={!show[1]} onClick={itemOverlay}>{UriToScreenFormat('overlay')}{show[1]}</button>
-                <button hidden={show[1]} onClick={()=>remove(1, overlay)}>{UriToScreenFormat('rm overlay')}{show[1]}</button>
+                <button onClick={()=>itemOverlay(0)}>{show[1] ? UriToScreenFormat('overlay') : UriToScreenFormat('rm overlay')}</button>
+                <button onClick={()=>itemOverlay(1)}>{show[1] ? UriToScreenFormat('overlay') : UriToScreenFormat('rm overlay')}</button>
                 <button hidden={!show[2]} onClick={itemPolygon}>{UriToScreenFormat('polygon')}{show[2]}</button>
                 <button hidden={show[2]} onClick={()=>remove(2, polygon)}>{UriToScreenFormat('rm polygon')}{show[2]}</button>
             </div>
             <div className='chart'>
-                <li><img src='/public/chart/25110.png'/></li>
+                {/* <li aria-disabled={!show[1]} onClick={itemOverlay}><img src='/public/chart/25110.png'/></li> */}
                 <li><img src='/public/chart/25110.png'/></li>
                 <li><img src='/public/chart/25110.png'/></li>
             </div>
