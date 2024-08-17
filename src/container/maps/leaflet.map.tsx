faf7443e3ada1b506ea91dd83c64d279ca343158
GadelhaTI <gadelha.ti@gmail.com>
2024-08-17 17:07:49 -0300

    feature: add hook usePoint

--- src/container/maps/leaflet.map.tsx
+++ src/container/maps/leaflet.map.tsx
@@ -9,6 +9,7 @@ import './leaflet.css'
 import { Chart } from '../../component/chart';
 import { MaritimeArea } from '../../component/maritime_area';
 import { initialPoint, Point } from '../../component/point';
+import { usePoint } from './useCoordinates';
 
 export const LeafletMap = () => {
     const center: L.LatLngExpression = [-23, -43]
@@ -18,12 +19,9 @@ export const LeafletMap = () => {
     const [ overlays ] = useState<L.ImageOverlay[]>([])
     const [ charts, setCharts ] = useState<Chart[]>([])
     const [ maritimeArea, setMaritimeArea ] = useState<MaritimeArea[]>([])
-    const [ point, setPoint ] = useState<Point>(initialPoint)
-    
-    const [ pointList, setPointList ] = useState<Point[]>([])
     const [ polygon, setPolygon ] = useState<L.Polygon>()
     const [ show, setShow ] = useState<boolean[]>([true, true, true, true, true, true, true, true, true])
-    // const [ tile, setTile ] = useState<boolean>(true)
+    
     let item: string = '/chart/'
 
     useEffect(()=>{
@@ -120,31 +118,6 @@ export const LeafletMap = () => {
         let audio = new Audio("/assets/sound/click_sound.mp3")
         audio.play()
     }
-    const handleInput = (event: ChangeEvent<HTMLInputElement>) => {
-        // const value = event.target.type === 'checkbox' ? event.target.checked : event.target.value
-        // if (new RegExp(event.target.pattern).test(event.target.value) || event.target.value === '') { 
-        setPoint({...point, [event.target.name]: event.target.value})
-        // }
-    }
-    const handleInputLatitude = (event: ChangeEvent<HTMLInputElement>) => {
-        // point.coordinates.splice(1, 0, Number(event.target.value))
-        // setPoint({...point, [event.target.name]: point.coordinates}
-        setPoint({...point, [event.target.name]: [point.coordinates[0], Number(event.target.value)]})
-    }
-    const handleInputLongitude = (event: ChangeEvent<HTMLInputElement>) => {
-        setPoint({...point, [event.target.name]: [event.target.value, point.coordinates[1]]})
-    }
-    const handleStates = (point: Point) => {
-        setPointList([...pointList, point])
-    }
-    const handleInputLatitudeIndex = (event: ChangeEvent<HTMLInputElement>, index: number) => {
-        setPointList([...pointList, 
-            {...point, [event.target.name]: [point.coordinates[0], Number(event.target.value)]}
-        ])
-    }
-    const handleInputLongitudeIndex = (event: ChangeEvent<HTMLInputElement>, index: number) => {
-        setPoint({...point, [event.target.name]: [event.target.value, point.coordinates[1]]})
-    }
     const addItemPolygon = (map: L.Map, points: Point[]) => {
         toggleShow(8)
         let list:[number, number][] = []
@@ -152,50 +125,47 @@ export const LeafletMap = () => {
             list.push([point.coordinates[1], point.coordinates[0]])
         })
         setPolygon(addPolygon(map, L.polygon(list, {color: 'red'})))
-        // setPointList1(addPolygon(map, L.polygon(list, {color: 'red'})))
     }
+    const { pointList, addPoint, setOrder, handleInputLongitude, handleInputLatitude, removePoint } = usePoint()
     return (
         <div>
             <div id='map' style={{ height: '100vh', width: '100vw' }}></div>
             <dialog open className='newdialog'>
-                <button onClick={()=>addPoint(map, point.coordinates[1], point.coordinates[0], 'gadelha')}>Map Point</button>
-                <button onClick={()=>handleStates(point)}>Add Point</button>
+                <button onClick={()=>addPoint()}>add ➕</button>
+                {/* <button onClick={()=>addPoint(map, point.coordinates[1], point.coordinates[0], 'gadelha')}>Map Point</button> */}
+                {/* <button onClick={handleStates}>➕</button> */}
+                <button onClick={removePoint}>rm</button>
                 <button onClick={()=>addPointList(map, pointList)}>Show List</button>
-                <button hidden={!show[8]} onClick={()=>addItemPolygon(map, pointList)}>{UriToScreenFormat('new polygon')}</button>
-                <button hidden={show[8]} onClick={()=>remove(8, polygon)}>{UriToScreenFormat('rm polygon')}</button>
-
-                <input name='coordinates' value={point.coordinates[0]} onChange={handleInputLongitude}></input>
-                <input name='coordinates' value={point.coordinates[1]} onChange={handleInputLatitude} type='number'></input>
+                <button onClick={show[8]? ()=>addItemPolygon(map, pointList) : ()=>remove(8, polygon)} >
+                    {UriToScreenFormat(show[8]?'new polygon':'rm polygon')}
+                </button>
                 <ol>
                 {pointList.map((element: Point, index: number)=>{
-                    return <li>
+                    return <li onFocus={()=>setOrder(index)}>
                         <input name='coordinates' value={element.coordinates[0]} onChange={handleInputLongitude}></input>
                         <input name='coordinates' value={element.coordinates[1]} onChange={handleInputLatitude}></input>
-                        {element.coordinates[0]}{element.coordinates[1]}
                         </li>
                 })}
                 </ol>
-                <>{point.coordinates[0]}, {point.coordinates[1]}</>
             </dialog>
             <div className='menu'>
-                <button hidden={!show[0]} onClick={()=>retrieveItem('gaugeStation', 'title')}>{UriToScreenFormat('gauge')}{show[0]}</button>
-                <button hidden={show[0]} onClick={()=>remove(0, markers)}>{UriToScreenFormat('rm gauge')}{show[0]}</button>
-                {/* <button onClick={()=>retrieveChart(0)}>{show[1] ? UriToScreenFormat('overlay') : UriToScreenFormat('overlay 0')}</button>
-                <button onClick={()=>retrieveChart(1)}>{show[1] ? UriToScreenFormat('overlay') : UriToScreenFormat('overlay 1')}</button> */}
+                <button onClick={show[0] ? ()=>retrieveItem('gaugeStation', 'title') : ()=>remove(0, markers) }>
+                    {UriToScreenFormat(show[0] ? 'gauge' : 'rm gauge')}{show[0]}
+                </button>
                 <button hidden={!show[2]} onClick={itemPolygon}>{UriToScreenFormat('polygon')}{show[2]}</button>
                 <button hidden={show[2]} onClick={()=>remove(2, polygon)}>{UriToScreenFormat('rm polygon')}{show[2]}</button>
                 <button hidden={!show[3]} onClick={itemPolygons}>{UriToScreenFormat('polygons')}{show[3]}</button>
                 <button hidden={show[3]} onClick={()=>remove(3, polygon)}>{UriToScreenFormat('rm polygons')}{show[3]}</button>
                 <button hidden={!show[4]} onClick={itemPolygons}>{UriToScreenFormat('area')}{show[4]}</button>
-                <button hidden={show[4]} onClick={()=>remove(4, polygon)}>{UriToScreenFormat('rm area')}{show[4]}</button>
+                <button hidden={show[4]} onClick={()=>remove(4, polygon)}>{UriToScreenFormat('rm area')}{show[4]}➕</button>
             </div>
             <div className='chart'>
                 {charts.map((element:any, index:number)=>{
                     if(index < 3) {
                         return <li className='menuitem' aria-disabled={!show[1]} onClick={()=>retrieveChart(index)}>
-                                <label>{element.number}</label>
-                                <img src={item.concat(element.number).concat('.png')}/>
-                            </li>
+                            <label>{element.number}</label>
+                            <img src={item.concat(element.number).concat('.png')}/>
+                        </li>
                     }
                 })}
             </div>
