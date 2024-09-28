8bc8d911503aa695a0c2fcb2127a34d02f5a1366
GadelhaTI <gadelha.ti@gmail.com>
2024-09-28 02:55:34 -0300

    chore: refactoring

--- src/assets/hook/usePosition.tsx
+++ src/assets/hook/usePosition.tsx
@@ -1,7 +1,15 @@
 import { useEffect, useState } from "react"
+// https://html-css-js.com/css/generator/transform/
+// https://css-tricks.com/almanac/properties/t/transform/#rotate
+// https://kushagra.dev/blog/css-only-3d-card/
+// https://polypane.app/css-3d-transform-examples/
 
 export const usePosition = () => {
-    const [ position, setPosition] = useState<{ x: number, y: number }>({ x:0, y:0 })
+    const [ movement, setMovement ] = useState({
+        backgroundColor: 'lightblue',
+        transform: `perspective(1000px) rotateX(0deg)`,
+        boxShadow: `0px 0px 21px 5px rgba(0,0,0,0.54)`,
+    })
     const controller = new AbortController();
     const box = document.querySelector("body") as HTMLInputElement | null
 
@@ -10,7 +18,7 @@ export const usePosition = () => {
         return (()=>{
             controller.abort()
         })
-    }, [position])
+    }, [movement])
     const callParalax = () => {
         if (box !== null) {
             box.addEventListener("mousemove", setParalax, false);
@@ -18,8 +26,14 @@ export const usePosition = () => {
     }
     const setParalax = (event: any) => {
         if (box !== null) {
-            setPosition({ x: Math.floor((event.pageX * 100)/box?.offsetWidth), y: Math.floor((event.pageY * 100)/box?.offsetHeight) })
+            let abcissa: number = Math.floor((event.pageX * 100)/box?.offsetWidth -50)
+            let ordenada: number = Math.floor((event.pageY * 100)/box?.offsetHeight -50)
+            setMovement({
+                backgroundColor: `rgb(${event.clientX % 255}, ${event.clientY % 255}, 150)`,
+                transform: `perspective(1000px) rotateY(${abcissa}deg) rotateX(${ordenada*-1}deg)`,
+                boxShadow: `${abcissa*-1}px ${ordenada*-1}px 21px 5px rgba(0,0,0,0.54)`,
+            });
         }
     }
-    return { position }
+    return { movement }
 }
--- src/container/data/GenericComponent.tsx
+++ src/container/data/GenericComponent.tsx
@@ -1,14 +1,11 @@
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 import { useRequest } from "../../assets/hook/useRequest"
 import { intialSearch, Search } from "../../component/search"
 import { DataTable } from "./DataTable";
 import { useInput } from "../../assets/hook/useInput";
 import '../template/inputgroup.css'
-import { usePosition } from "../../assets/hook/usePosition";
 import './position.css'
-// https://html-css-js.com/css/generator/transform/
-// https://css-tricks.com/almanac/properties/t/transform/#rotate
-// https://kushagra.dev/blog/css-only-3d-card/
+import { usePosition } from "../../assets/hook/usePosition";
 
 interface Data<T extends Object> {
     object: T,
@@ -20,66 +17,14 @@ export const GenericComponent = <T extends Object>(object: Data<T>) => {
     // const [isInterface] = useIsInterface<T, User>(initialRole, initialUser)
     const { state: search, handleInput: handleSearch } = useInput<Search>(intialSearch)
     const { states, pageable, retrieve } = useRequest<T>(object.url, search.value, search.page, search.size, { key: search.key, order: search.order })
-    // const { position } = usePosition()
-    const [ position, setPosition] = useState<{ x: number, y: number }>({ x:0, y:0 })
-    const box = document.querySelector("body") as HTMLInputElement | null
-    const [ movement, setMovement ] = useState({
-        backgroundColor: 'lightblue',
-        // height: `${position.x}%`,
-        // width: `${position.y}%`,
-        transform: `perspective(1000px) rotateX(${position.y}deg)`,
-        boxShadow: `${position.x}px ${position.y}px 21px 5px rgba(0,0,0,0.54)`,
-    })
+    const { movement } = usePosition()
 
     useEffect(() => {
         retrieve(object.url)
-        move()
-        callParalax()
         return (() => {
             controller.abort()
         })
     }, [search])
-    const callParalax = () => {
-        if (box !== null) {
-            box.addEventListener("mousemove", setParalax, false);
-        }
-    }
-    const setParalax = (event: any) => {
-        if (box !== null) {
-            let abcissa: number = Math.floor((event.pageX * 100)/box?.offsetWidth -50)
-            let ordenada: number = Math.floor((event.pageY * 100)/box?.offsetHeight -50)
-
-            setPosition({ x: ordenada/2, y: abcissa/2 })
-            setMovement({
-                backgroundColor: `rgb(${event.clientX % 255}, ${event.clientY % 255}, 150)`,
-                // height: `${position.x}%`,
-                // width: `${position.y}%`,
-                // transform: `skew(${position.y}rad)`,
-                transform:
-                    `perspective(1000px) rotateY(${abcissa}deg) rotateX(${ordenada*-1}deg)`,
-                boxShadow: `${abcissa*-1}px ${ordenada*-1}px 21px 5px rgba(0,0,0,0.54)`,
-            });
-        }
-    }
-    // const handleMouseMove = (event: React.MouseEvent<HTMLDialogElement>) => {
-    //     setMovement({
-    //         backgroundColor: `rgb(${event.clientX % 255}, ${event.clientY % 255}, 150)`,
-    //         // height: `${position.x}%`,
-    //         // width: `${position.y}%`,
-    //         // transform: `skew(${position.y}rad)`,
-    //         transform: `rotateX(${position.y}deg) rotateY(-${position.x}deg)`
-    //     });
-    // };
-    const move = () => {
-        let box1 = document.querySelector("body") as HTMLInputElement | null
-        // box1?.setAttribute("style", `height: ${position.x}%`);
-        if(box1?.style !== null && box1?.style.height !== null) {
-            // box1?.style?.height = position.x
-        }
-        // box1?.setAttribute("style", `color: red`);
-        // let elem = document?.getElementById('dial') as HTMLInputElement | null
-        // setStyleAttribute(elem, {'font-size':'12px', color: 'red', 'margin-top': '5px'});
-    }
     const showModal = () => {
         (document.querySelector('#dial') as HTMLDialogElement).showModal()
     }
@@ -97,13 +42,9 @@ export const GenericComponent = <T extends Object>(object: Data<T>) => {
                     <label htmlFor={key}>{key}</label>
                 </span>
             })}
-            {/* <dialog open={true} id="dial" style={{ height: `${position.x}%` }} > */}
-            {/* <dialog open={true} id="dial" onMouseMove={(event)=>handleMouseMove(event)} style={{...movement}} > */}
             <dialog id="dial" style={{...movement}} >
-                <div /*style={{...movement}} */>
+                <div>
                     {JSON.stringify(movement)}
-                    {JSON.stringify(position.x)}{" | "}
-                    {JSON.stringify(position.y)}
                 </div>
             </dialog>
             <button onClick={showModal}>dialogue</button>
