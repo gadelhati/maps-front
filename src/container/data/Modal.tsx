fc7db26ee754deb26b8d02a3c16b2f5e31cd7a9c
GadelhaTI <gadelha.ti@gmail.com>
2024-10-10 03:16:15 -0300

    refactor: new modal to confirm crud

--- src/container/data/Modal.tsx
+++ src/container/data/Modal.tsx
@@ -3,6 +3,7 @@ import { useInput } from '../../assets/hook/useInput'
 import { create, update } from '../../service/service.crud'
 import { useRequest } from '../../assets/hook/useRequest'
 import './modal.css'
+import { UriToScreenFormat } from '../../assets/uri.format'
 
 export interface ModalData {
     showModal: () => void
@@ -19,7 +20,6 @@ const Modal = <T extends Object>(data: Data<T>, ref: Ref<ModalData>) => {
     const { state, setState, handleInput, handleSelect } = useInput<T>(data.object)
     const { states, retrieve } = useRequest<T>('url')
     const [action, setAction] = useState<string>('retrieve')
-    const [sure, setSure] = useState<boolean>(false)
     // const { states, pageable, retrieve } = useRequest<T>(object.url, search.value, search.page, search.size, { key: search.key, order: search.order })
 
     useEffect(() => {
@@ -27,10 +27,16 @@ const Modal = <T extends Object>(data: Data<T>, ref: Ref<ModalData>) => {
     }, [data.object])
 
     const showModal = () => {
-        (document.querySelector('#dialog') as HTMLDialogElement).showModal()
+        (document.querySelector(`#selected`) as HTMLDialogElement).showModal()
+    }
+    const showConfirm = () => {
+        (document.querySelector(`#confirm`) as HTMLDialogElement).showModal()
     }
     const closeModal = () => {
-        (document.querySelector('#dialog') as HTMLDialogElement).close()
+        const elements = Array.from(document.querySelectorAll(`#selected, #confirm`))
+        elements.forEach((element: any)=> {
+            element.close()
+        })
     }
     useImperativeHandle(ref, () => ({
         showModal, closeModal
@@ -38,14 +44,14 @@ const Modal = <T extends Object>(data: Data<T>, ref: Ref<ModalData>) => {
     const crud = (action: string) => {
         setAction(action)
         closeModal()
-        setSure(true)
+        showConfirm()
     }
     const confirmCrud = () => {
         switch (action) {
-            case 'create': create(data.url, state); setSure(false); break
-            case 'update': update(data.url, state); setSure(false); break
-            // case 'delete': remove(data.url, state?.id); setSs(false); break
-            default: setSure(false);
+            case 'create': create(data.url, state); closeModal(); break
+            case 'update': update(data.url, state); closeModal(); break
+            // case 'delete': remove(data.url, state?.id); closeModal(); break
+            default: closeModal();
         }
     }
     const fill = (uri: string) => {
@@ -53,32 +59,46 @@ const Modal = <T extends Object>(data: Data<T>, ref: Ref<ModalData>) => {
     }
     return (
         <>
-            <dialog id="dialog">
-                {data.object !== undefined && Object.entries(state).map(([key, value]: [string, any]) => {
-                    return <span key={key + 'span'} className={'inputgroup tooltip'} data-tip={[]} style={{ display: 'flex' }}>
-                        {typeof value === 'object' ?
-                            // {/* {Array.isArray(value) ? */}
-                            <select key={key} name={key} onClick={() => fill(key)} onChange={handleSelect}
-                                defaultValue={value === undefined || value === null ? null : Array.isArray(value) && value[0] !== undefined ? (value[0].hasOwnProperty('name') ? value[0]?.name : value[0]?.id) : value.name !== undefined ? value?.name : value?.id}>
-                                <option selected value={value === undefined || value === null ? null : Array.isArray(value) ? value[0] : value}>
-                                    {value === undefined || value === null ? null : Array.isArray(value) && value[0] !== undefined ? (value[0].hasOwnProperty('name') ? value[0]?.name : value[0]?.id) : value.name !== undefined ? value?.name : value?.id}
-                                </option>
-                                {states?.map(((result: any) => <option key={Math.random()} value={result.id}>{result?.name ? result.name : result.id}</option>))}
-                            </select>
-                            :
-                            <input key={key} type={typeof value} name={key} value={value} onChange={handleInput} placeholder={key} ></input>
-                        }
-                        <label htmlFor={key}>{key}</label>
-                    </span>
-                })}
-                <button onClick={() => crud('create')}>Create</button>
-                <button onClick={() => crud('update')}>Update</button>
-                {/* <button onClick={() => crud('delete')}>Delete</button> */}
-                <button onClick={closeModal}>Close</button>
+            <dialog id='selected' className="dialog">
+                <header>
+                    <h2>{UriToScreenFormat(data.url)}</h2>
+                    <span onClick={closeModal}>&times;</span>
+                </header>
+                <center>
+                    {data.object !== undefined && Object.entries(state).map(([key, value]: [string, any]) => {
+                        return <span key={key + 'span'} className={'inputgroup tooltip'} data-tip={[]} style={{ display: 'flex' }}>
+                            {typeof value === 'object' ?
+                                // {/* {Array.isArray(value) ? */}
+                                <select key={key} name={key} onClick={() => fill(key)} onChange={handleSelect}
+                                    defaultValue={value === undefined || value === null ? null : Array.isArray(value) && value[0] !== undefined ? (value[0].hasOwnProperty('name') ? value[0]?.name : value[0]?.id) : value.name !== undefined ? value?.name : value?.id}>
+                                    <option selected value={value === undefined || value === null ? null : Array.isArray(value) ? value[0] : value}>
+                                        {value === undefined || value === null ? null : Array.isArray(value) && value[0] !== undefined ? (value[0].hasOwnProperty('name') ? value[0]?.name : value[0]?.id) : value.name !== undefined ? value?.name : value?.id}
+                                    </option>
+                                    {states?.map(((result: any) => <option key={Math.random()} value={result.id}>{result?.name ? result.name : result.id}</option>))}
+                                </select>
+                                :
+                                <input key={key} type={typeof value} name={key} value={value} onChange={handleInput} placeholder={key} ></input>
+                            }
+                            <label htmlFor={key}>{key}</label>
+                        </span>
+                    })}
+                </center>
+                <footer>
+                    <button onClick={() => crud('create')}>Create</button>
+                    <button onClick={() => crud('update')}>Update</button>
+                    {/* <button onClick={() => crud('delete')}>Delete</button> */}
+                    <button onClick={closeModal}>Close</button>
+                </footer>
             </dialog>
-            <dialog open={sure}>
-                <button onClick={confirmCrud}>Confirmar</button>
-                <button onClick={() => setSure(false)}>Close</button>
+            <dialog id='confirm' className='dialog'>
+                <header>
+                    <h2>{UriToScreenFormat(action)}</h2>
+                    <span onClick={closeModal}>&times;</span>
+                </header>
+                <footer>
+                    <button onClick={confirmCrud}>Confirmar</button>
+                    <button onClick={closeModal}>Close</button>
+                </footer>
             </dialog>
         </>
     )
