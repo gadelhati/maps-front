431131dd7bc06f35d03f7ba686a837f44f834a70
GadelhaTI <gadelha.ti@gmail.com>
2024-10-11 00:08:42 -0300

    refactor: change component file

--- src/component/person.tsx
+++ src/component/person.tsx
@@ -21,3 +21,35 @@ export const initialPerson: Person = {
     country: initialCountry,
     address: [],
 }
+
+export interface NaturalPerson extends Person {
+    cpf: string,
+    rg: string,
+}
+
+export const initialNaturalPerson: NaturalPerson = {
+    id: '',
+    name: '',
+    birth: new Date(),
+    email: '',
+    telephone: [],
+    country: initialCountry,
+    address: [],
+    cpf: '',
+    rg: '',
+}
+
+export interface LegalEntity extends Person {
+    cnpj: string,
+}
+
+export const initialLegalEntity: LegalEntity = {
+    id: '',
+    name: '',
+    birth: new Date(),
+    email: '',
+    telephone: [],
+    country: initialCountry,
+    address: [],
+    cnpj: '',
+}
