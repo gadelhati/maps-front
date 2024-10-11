0965ffe94035894b688f92ca599e67ba9c2848b5
GadelhaTI <gadelha.ti@gmail.com>
2024-10-11 00:07:32 -0300

    refactor: remove components

--- src/container/data/GenericComponent.tsx
+++ src/container/data/GenericComponent.tsx
@@ -4,8 +4,6 @@ import { intialSearch, Search } from "../../component/search"
 import { DataTable } from "./DataTable";
 import { useInput } from "../../assets/hook/useInput";
 import '../template/inputgroup.css'
-import { usePosition } from "../../assets/hook/usePosition";
-// https://www.cssportal.com/css-clip-path-generator/
 
 interface Data<T extends Object> {
     object: T,
@@ -17,7 +15,6 @@ export const GenericComponent = <T extends Object>(object: Data<T>) => {
     // const [isInterface] = useIsInterface<T, User>(initialRole, initialUser)
     const { state: search, handleInput: handleSearch } = useInput<Search>(intialSearch)
     const { states, pageable, retrieve } = useRequest<T>(object.url, search.value, search.page, search.size, { key: search.key, order: search.order })
-    const { movement } = usePosition()
 
     useEffect(() => {
         retrieve(object.url)
@@ -25,9 +22,6 @@ export const GenericComponent = <T extends Object>(object: Data<T>) => {
             controller.abort()
         })
     }, [search])
-    const showModal = () => {
-        (document.querySelector('#dial') as HTMLDialogElement).showModal()
-    }
     return (
         <>
             {/* <select key={Math.random()} name={'key'} onChange={handleInputChange} value={search.key} >
@@ -42,13 +36,6 @@ export const GenericComponent = <T extends Object>(object: Data<T>) => {
                     <label htmlFor={key}>{key}</label>
                 </span>
             })}
-            <dialog id="dial" style={{...movement}} >
-                <>Teste</>
-                <div style={{ boxShadow: movement.boxShadow }}>
-                    {JSON.stringify(movement)}
-                </div>
-            </dialog>
-            <button onClick={showModal}>dialogue</button>
             <DataTable object={object.object} list={states} pageable={pageable} search={search} url={object.url} />
         </>
     )
