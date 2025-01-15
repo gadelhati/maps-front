2d788214f2b1879b9d99db10b895b1e31199521e
GadelhaTI <gadelha.ti@gmail.com>
2025-01-14 22:03:24 -0300

    chore: organize hook

--- src/assets/hook/useProvider.tsx
+++ src/assets/hook/useProvider.tsx
@@ -1,9 +1,13 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useEffect, useState, useContext } from "react";
 import { getToken, isValidToken } from "../../service/service.token";
 import { initialAuth } from "../../component/auth";
 import { Auth } from "../../component/auth";
 
 export const AuthContext = createContext<Auth>({} as Auth);
+
+export const useAuth = () => {
+    return useContext(AuthContext);
+}
 
 export const AuthProvider = ({ children }: any) => {
     const [state, setState] = useState<Auth>(initialAuth)
