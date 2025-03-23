c3e853de0df5fb092660e501b927d3045f7c46bf
GadelhaTI <gadelha.ti@gmail.com>
2025-03-22 21:37:59 -0300

    refactor: dry on request

--- src/assets/hook/useRequest.tsx
+++ src/assets/hook/useRequest.tsx
@@ -20,30 +20,21 @@ export const useRequest = <T extends Object>(url: string, search: Search) => {
     const request = useCallback(async (url: string, search?: Search, signal?: AbortSignal ) => {
         if(search?.page === undefined && search?.size === undefined){
             return await api.get<T>(`/${url}`)
-            .then((response: any) => {
-                if (response.data.content.length !== 0) {
-                    setStates(response.data.content)
-                    setPageable(response.data.page)
-                }
-            })
+            .then((response: any) => { setObject(response) })
             .catch(error => { return setError(error) })
         } else if (search?.sort?.order === undefined) {
             return await api.get<T>(`/${url}?value=${search?.value}`, { params: { page: search?.page, size: search?.size }, signal } )
-            .then((response: any) => {
-                if (response.data.content.length !== 0) {
-                    setStates(response.data.content)
-                    setPageable(response.data.page)
-                }
-            })
+                .then((response: any) => { setObject(response) })
             } else {
                 return await api.get<T>(`/${url}?value=${search?.value}`, { params: { page: search?.page, size: search?.size, sort: `${search?.sort?.key},${search?.sort?.order}` }, signal } )
-                .then((response: any) => {
-                    if (response.data.content.length !== 0) {
-                        setStates(response.data.content)
-                        setPageable(response.data.page)
-                    }
-                })
+                .then((response: any) => { setObject(response) })
             }
     }, [url, search])
+    const setObject = (response: any) => {
+        if (response.data.content.length !== 0) {
+            setStates(response.data.content)
+            setPageable(response.data.page)
+        }
+    }
     return { states, pageable, error, request }
 }
