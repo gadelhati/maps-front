317236a70da684576373fa8ac63fd78956dd594b
1SG-HN Gadelha <marcelo.gadelha@mairnha.mil.br>
2025-04-17 08:28:32 -0300

    chore: remove prop types.

--- src/assets/image/svg.access.tsx
+++ src/assets/image/svg.access.tsx
@@ -1,18 +1,18 @@
 import menuIcon from "../image/menuIcon.svg";
-import PropTypes from 'prop-types';
 import './svg.acess.css'
+
 interface Icon {
     name: string,
+    color?: string,
+    size?: number,
 }
 
-export const Icon = (icon: Icon) => (
-    <svg className={'svg'}>
-        <use xlinkHref={`${menuIcon}#${icon.name}`} />
+export const Icon = ({ name, color = 'currentColor', size = 24 }: Icon) => (
+    <svg className={'svg'} style={{
+        fill: color || 'currentColor',
+        width: size || 24,
+        height: size || 24,
+      }}>
+        <use xlinkHref={`${menuIcon}#${name}`} />
     </svg>
-);
-
-Icon.propTypes = {
-    name: PropTypes.string.isRequired,
-    color: PropTypes.string,
-    size: PropTypes.number
-};
+)
