8905942fad3654d454a16f2ea7d49e9719b758d0
GadelhaTI <gadelha.ti@gmail.com>
2025-05-11 11:38:30 -0300

    style: cleaning up input

--- src/container/login.tsx
+++ src/container/login.tsx
@@ -1,6 +1,6 @@
 import { GInput } from './data/GInput';
 import { GButton } from './data/GButton';
-import './login.css'
+// import './login.css'
 
 export const Login = () => {
     return (
@@ -10,9 +10,9 @@ export const Login = () => {
                     <h2>Bem vindo de volta</h2>
                     <p>Entre com suas credenciais</p>
                 </div>
-                <GInput name='username' resource='fas fa-user form-group__icon' ></GInput>
-                <GInput name='password' resource='fas fa-lock form-group__icon' type='password' ></GInput>
-                <GInput name='totpKey' resource='fas fa-key form-group__icon' ></GInput>
+                <GInput name='username' resource='fas fa-user' ></GInput>
+                <GInput name='password' resource='fas fa-lock' type='password' ></GInput>
+                <GInput name='totpKey' resource='fas fa-key' ></GInput>
                 <input type="hidden" id="captchaToken" name="captchaToken" />
                 <GButton type="submit" className="submit-button">Entrar</GButton>
                 <div className="login-footer">
