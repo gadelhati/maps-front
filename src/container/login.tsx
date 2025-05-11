ca425d0b31396b2a4c5fbc143d2ee53e61410cf3
GadelhaTI <gadelha.ti@gmail.com>
2025-05-11 03:15:04 -0300

    style: login page

--- src/container/login.tsx
+++ src/container/login.tsx
@@ -4,25 +4,27 @@ import './login.css'
 
 export const Login = () => {
     return (
-        <>
-            <div className="login-header">
-                <h2>Bem vindo de volta</h2>
-                <p>Entre com suas credenciais</p>
-            </div>
-            <GInput name='username' resource='fas fa-user form-group__icon' ></GInput>
-            <GInput name='password' resource='fas fa-lock form-group__icon' type='password' ></GInput>
-            <GInput name='totpKey' resource='fas fa-key form-group__icon' ></GInput>
-            <input type="hidden" id="captchaToken" name="captchaToken" />
-            <GButton type="submit" className="submit-button">Entrar</GButton>
-            <div className="login-footer">
-			<p>Não tem uma conta? <a href="@{/register}">Cadastre-se</a></p>
-            </div>
-            <div className="login-footer">
-                <p><a href="@{/resetPassword}">Esqueci minha senha</a></p>
-            </div>
-            <div className="login-footer">
-                <p><a href="@{/resetTotp}">Esqueci minha semente</a></p>
-            </div>
-        </>
+        <section className="login-container">
+            <article className="login-box">
+                <div className="login-header">
+                    <h2>Bem vindo de volta</h2>
+                    <p>Entre com suas credenciais</p>
+                </div>
+                <GInput name='username' resource='fas fa-user form-group__icon' ></GInput>
+                <GInput name='password' resource='fas fa-lock form-group__icon' type='password' ></GInput>
+                <GInput name='totpKey' resource='fas fa-key form-group__icon' ></GInput>
+                <input type="hidden" id="captchaToken" name="captchaToken" />
+                <GButton type="submit" className="submit-button">Entrar</GButton>
+                <div className="login-footer">
+                    <p>Não tem uma conta? <a href="@{/register}">Cadastre-se</a></p>
+                </div>
+                <div className="login-footer">
+                    <p><a href="@{/resetPassword}">Esqueci minha senha</a></p>
+                </div>
+                <div className="login-footer">
+                    <p><a href="@{/resetTotp}">Esqueci minha semente</a></p>
+                </div>
+            </article>
+        </section>
     )
 }
